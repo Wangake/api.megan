a171068de97e0a077b2c9a0120102cc37a1cb12c
Wangake <youngwanga254@gmail.com>
2026-01-10 18:31:23 +0300

    Create fetchWrapper.js

---
+++ utils/fetchWrapper.js
@@ -0,0 +1,28 @@
+// Wrapper for EliteProTech API calls
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const ELITE_BASE = "https://eliteprotech-apis.zone.id";
+
+async function fetchFromElite(endpoint, params = {}) {
+    try {
+        const queryString = new URLSearchParams(params).toString();
+        const url = `${ELITE_BASE}${endpoint}${queryString ? '?' + queryString : ''}`;
+        
+        console.log(`[PROXY] Fetching: ${url}`);
+        
+        const response = await fetch(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0',
+                'Accept': 'application/json',
+                'Accept-Language': 'en-US,en;q=0.9'
+            }
+        });
+        
+        return await response.json();
+    } catch (error) {
+        console.error(`[PROXY ERROR] ${endpoint}:`, error.message);
+        return { success: false, error: error.message };
+    }
+}
+
+module.exports = { fetchFromElite };
